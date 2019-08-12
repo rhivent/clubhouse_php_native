464c8f72d89a398958908d610a98a581f2da19c9
rhivent <zzzzjrjr@gmail.com>
2019-08-12 21:19:04 +0700

    commit sql dump baru

--- clubhouse.sql
+++ clubhouse.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 11, 2019 at 04:51 AM
+-- Generation Time: Aug 12, 2019 at 02:16 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.2.19
 
@@ -52,7 +52,7 @@ INSERT INTO `jenis_member` (`id_jenismember`, `kode_jenis_member`, `nama_jenis_m
 
 CREATE TABLE `jenis_paket` (
   `id_jenis_paket` int(11) NOT NULL,
-  `name_jenis_paket` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama_jenis_paket` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `harga` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `terbilang` text COLLATE utf8mb4_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -61,7 +61,7 @@ CREATE TABLE `jenis_paket` (
 -- Dumping data for table `jenis_paket`
 --
 
-INSERT INTO `jenis_paket` (`id_jenis_paket`, `name_jenis_paket`, `harga`, `terbilang`) VALUES
+INSERT INTO `jenis_paket` (`id_jenis_paket`, `nama_jenis_paket`, `harga`, `terbilang`) VALUES
 (1, 'Harian', '15000', 'Lima Belas Ribu Rupiah'),
 (2, '1 Bulan', '450000', 'Empat Ratus Lima Puluh Ribu Rupiah'),
 (3, '2 Bulan', '800000', 'Delapan Ratus Ribu Rupiah'),
@@ -95,8 +95,13 @@ CREATE TABLE `tbl_member_umum` (
 
 INSERT INTO `tbl_member_umum` (`id_umum`, `nomor_kartu`, `nama_umum`, `barcode`, `status`, `jenis_member`, `jenis_paket`, `alamat`, `tgl_lahir`, `foto_umum`, `biaya`, `tgl_aktifitas`, `masa_berlaku`) VALUES
 (18, '34152', 'asdasdk ', 'qPzCG4pSTn', 'active', 'B1', 2, 'asdmakdmas damksd', '2019-08-06', '1565497777_testgmbar.jpg', '450000', '2019-08-11 00:00:00', '2019-09-10'),
-(19, '74028', 'asdkamdm', 'NK4La3x5TB', 'active', 'B1', 4, 'asdasmdkamskdas', '2019-08-16', '1565498737_sftp key qovarcom.png', '1200000', '2019-08-11 00:00:00', '2019-11-09'),
-(20, '25942', 'asdkamdm', 'rhOaoQkdDz', 'active', 'B1', 4, 'asdasmdkamskdas', '2019-08-16', '1565498895_sftp key qovarcom.png', '1200000', '2019-08-11 00:00:00', '2019-11-09');
+(28, '25942', 'Master', 'rhOaoQkdDz', 'active', 'isidentil', 1, 'asdasmdkamskdas', '2019-08-16', '1565587081_3.jpg', '15000', '2019-08-11 00:00:00', '2019-08-13'),
+(36, '25942', 'asdkamdm', 'rhOaoQkdDz', 'active', 'B1', 4, 'asdasmdkamskdas', '2019-08-16', '1565590576_testgmbar.jpg', '1200000', '2019-08-11 00:00:00', '2019-11-10'),
+(48, '25942', 'asdkamdm', 'rhOaoQkdDz', 'active', 'B1', 4, 'asdasmdkamskdas', '2019-08-16', '1565614185_1.jpg', '1200000', '2019-08-11 00:00:00', '2019-11-10'),
+(56, '25942', 'asdkamdm', 'rhOaoQkdDz', 'active', 'B1', 4, 'asdasmdkamskdas', '2019-08-16', '1565498895_sftp key qovarcom.png', '1200000', '2019-08-11 00:00:00', '2019-11-09'),
+(57, '25942', 'asdkamdm', 'rhOaoQkdDz', 'active', 'B1', 4, 'asdasmdkamskdas', '2019-08-16', '1565498895_sftp key qovarcom.png', '1200000', '2019-08-11 00:00:00', '2019-11-09'),
+(58, '25942', 'asdkamdm', 'rhOaoQkdDz', 'active', 'B1', 4, 'asdasmdkamskdas', '2019-08-16', '1565498895_sftp key qovarcom.png', '1200000', '2019-08-11 00:00:00', '2019-11-09'),
+(59, '123123', 'asdkamdm', 'rhOaoQkdDz', 'active', 'B1', 4, 'asdasmdkamskdas', '2019-08-16', '1565498895_sftp key qovarcom.png', '1200000', '2019-08-11 00:00:00', '2019-11-09');
 
 -- --------------------------------------------------------
 
@@ -119,9 +124,42 @@ CREATE TABLE `tbl_member_warga` (
 --
 
 INSERT INTO `tbl_member_warga` (`id_warga`, `no_kavling`, `nama_warga`, `telp`, `tgl_daftar`, `barcode`, `foto_warga`) VALUES
-(1, 'M-12', 'makdmas mkasd', '9130123123', '2019-08-11 04:17:53', 'GItQrODocP', 'api key binance.png'),
-(2, 'M-12', 'akdmaskmd', '09123123', '2019-08-11 04:30:02', 'Ihj9HiSG1Q', '1565497802_Roadmap_qovar.io.PNG'),
-(3, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG');
+(9, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(10, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(11, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(12, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(13, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(14, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(15, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(16, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(17, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(18, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(19, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(20, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(21, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(22, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(23, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(24, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(25, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(26, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(27, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(28, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(29, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(30, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(31, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(32, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(33, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(34, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(35, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(36, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(37, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(38, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(39, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(40, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(41, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(42, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(43, 'ohohoh', 'amskdmaskdma', '10331', '2019-08-11 04:43:12', 'JiuB8SvXgq', '1565498592_Roadmap_qovar.io.PNG'),
+(45, 'm67', 'qww', '09877', '2019-08-12 12:40:06', 'RUA32xCnVF', '1565613606_1.jpg');
 
 --
 -- Indexes for dumped tables
@@ -171,13 +209,13 @@ ALTER TABLE `jenis_paket`
 -- AUTO_INCREMENT for table `tbl_member_umum`
 --
 ALTER TABLE `tbl_member_umum`
-  MODIFY `id_umum` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id_umum` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 
 --
 -- AUTO_INCREMENT for table `tbl_member_warga`
 --
 ALTER TABLE `tbl_member_warga`
-  MODIFY `id_warga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_warga` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
